5de006fa4f1fff5ac73ab118f0cfccfddc6966e6
Ömer Burak ŞAL <omerburaksal@Omer-MacBook-Air.local>
2026-04-25 14:24:30 +0300

    second project added. project is based on flutter documents

---
+++ birdle/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'game.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Align(
+            alignment: Alignment.center,
+            child: Text('Birdle'),
+          ),
+        ),
+        body: const Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Tile('A', HitType.hit),
+              Tile('B', HitType.miss),
+              Tile('C', HitType.partial),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          for (final guess in _game.guesses)
+            Row(
+              children: [
+                for (var i = 0; i < 5; i++)
+                  Tile(guess[i].char, guess[i].type),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
